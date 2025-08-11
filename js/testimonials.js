52741cabdf6a9f3875a2636ec1780b3c7178e20a
fatu-ops <fatima1.nisar@gmail.com>
2025-08-11 11:37:42 +0500

    Create testimonials.js

---
+++ js/testimonials.js
@@ -0,0 +1,23 @@
+const carousel = document.querySelector('.testimonial-carousel');
+let isDown = false;
+let startX;
+let scrollLeft;
+
+if(carousel) {
+  carousel.addEventListener('mousedown', e => {
+    isDown = true;
+    startX = e.pageX - carousel.offsetLeft;
+    scrollLeft = carousel.scrollLeft;
+  });
+
+  carousel.addEventListener('mouseleave', () => isDown = false);
+  carousel.addEventListener('mouseup', () => isDown = false);
+
+  carousel.addEventListener('mousemove', e => {
+    if (!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - carousel.offsetLeft;
+    const walk = (x - startX) * 3; // Scroll-fast multiplier
+    carousel.scrollLeft = scrollLeft - walk;
+  });
+}
